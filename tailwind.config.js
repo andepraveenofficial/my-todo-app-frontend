8043170c1d09d3d468148938a162df6efec1b293
Ande Praveen <praveenande2023@gmail.com>
2024-12-03 22:58:16 +0530

    storybook added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './stories/**/*.{js,jsx,ts,tsx,mdx}',
+    './.storybook/**/*.{js,jsx,ts,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
